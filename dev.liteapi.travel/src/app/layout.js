d3007ff9b155d488ed1dab6506c3caeb2c8ea68b
Mohamed Nawfal El Fineq <nawfal@Mohameds-MacBook-Pro.local>
2023-05-18 13:46:58 +0100

    spa done

---
+++ dev.liteapi.travel/src/app/layout.js
@@ -0,0 +1,27 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import LiteApiIcon from './(images)/liteapi.jpeg'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'dev.liteapi',
+
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+      </Head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
